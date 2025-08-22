eda1e327a363aad9337398d547b7027b20ec8b45
Rishikesh Karkhanis <rishikeshkarkhanis0101@gmail.com>
2025-08-22 11:49:58 +0530

    Login With Backend Done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,33 @@
 import "./Home.css";
+import { useEffect, useState } from 'react';
 
 function Home () {
+
+    const [user, setUser] = useState('');
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            const response = await fetch("/users/currentUser");
+            const json = await response.json();
+
+            if (!response.ok) {
+                window.location.href = "/login";
+                console.log(response);
+            }
+
+            setUser(json);
+            console.log(json);
+            console.log(response);
+        }
+
+        fetchUserData();
+    }, []);
+    
+
+
     return (
         <>
-            <h1>Home Page</h1>
+            <h1>{user ? `Hello, ${user._doc.username}` : 'Loading...'}</h1>
         </>
     );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,31 @@
 import "./Login.css";
+import { useState } from "react";
 
 function Login () {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    async function handleLogin () {
+        const user = {"email": email, "password": password};
+
+        const response = await fetch('/users/login', {
+            method: "POST",
+            body: JSON.stringify(user),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            console.log(response);
+            window.location.href = "/";
+        }
+
+        if (!response.ok) {
+            setError("Incorrect Username Or Password!");
+        }
+
+    }
 
     return (
         <>
@@ -10,12 +35,18 @@ function Login () {
                         <h1 className="pix">Pix</h1>
                         <h1 className="gram">Gram</h1>
                     </div>
-                    <form className="loginForm">
+
+                    <div className="loginForm">
                         <h2>Login</h2>
-                        <input type="email" placeholder="Email" required />
-                        <input type="password" placeholder="Password" required />
-                        <button type="submit">Login</button>
-                    </form>
+                        <input type="email" placeholder="Email" onChange={(e) => { setEmail(e.target.value); }} required />
+                        <input type="password" placeholder="Password" onChange={(e) => { setPassword(e.target.value); }} required />
+                        <button onClick={handleLogin}>Login</button>
+                    </div>
+
+                    <div className="error">
+                        {error && <p className="errorMessage">{error}</p>}
+                    </div>
+
                     <div className="loginSwitch">
                         <p>New To PixGram? : </p>
                         <a href="/register">Register</a>
