c041467ea56e6df2ed097da9465720235f5d599e
Rishikesh Karkhanis <rishikeshkarkhanis0101@gmail.com>
2025-08-22 14:30:50 +0530

    Login/Register Done

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,22 +3,56 @@ import { useRef, useState } from "react";
 
 
 function Register () {
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [profiePic, setProfilePic] = useState(
+        "https://storage.cloud.google.com/pixgram/User/ProfilePictures/default/default.webp"
+    );
+    const [error, setError] = useState("");
+
     const fileInputRef = useRef(null);
     const [imageSrc, setImageSrc] = useState(
-    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+    "https://storage.cloud.google.com/pixgram/User/ProfilePictures/default/default.webp"
   );
 
     const handleImageUpload = () => {
         fileInputRef.current.click();
     };
 
-    const handleFileChange = (event) => {
+    const handleFileChange = async (event) => {
         const file = event.target.files[0];
-        if (file) {
-            console.log("Selected file:", file);
-            setImageSrc(URL.createObjectURL(file));
-        }
+        if (!file) return;
+        
+        console.log("Selected file:", file);
+
+        setImageSrc(URL.createObjectURL(file));
     };
+
+    async function handleSubmit() {
+        const jsonData = {
+            username: username,
+            email: email,
+            password: password,
+            profilePicture: profiePic
+        };
+        
+        const response = await fetch('users/register', {
+            method: "POST",
+            body: JSON.stringify(jsonData),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            window.location.replace('/login')
+        }
+
+        if (!response.ok) {
+            setError('User Already Exists!')
+        }
+        
+    }
 
     return (
         <>
@@ -28,14 +62,19 @@ function Register () {
                         <h1 className="pix">Pix</h1>
                         <h1 className="gram">Gram</h1>
                     </div>
-                    <form className="registerForm">
+                    <div className="registerForm">
                         <h2>Register</h2>
                         <img className="image" srcSet={imageSrc} onClick={handleImageUpload} />
-                        <input type="text" placeholder="Username" required />
-                        <input type="email" placeholder="Email" required />
-                        <input type="password" placeholder="Password" required />
-                        <button type="submit">Register</button>
-                    </form>
+                        <input type="text" placeholder="Username" required onChange={(e) => { setUsername(e.target.value); }}/>
+                        <input type="email" placeholder="Email" required onChange={(e) => { setEmail(e.target.value); }}/>
+                        <input type="password" placeholder="Password" required onChange={(e) => { setPassword(e.target.value); }}/>
+                        <button onClick={handleSubmit}>Register</button>
+                    </div>
+
+                    <div className="error">
+                        {error && <p style={{ color: 'red' }}>{error}</p>}
+                    </div>
+
                     <div className="hiddenFile">
                         <input
                             type="file"
